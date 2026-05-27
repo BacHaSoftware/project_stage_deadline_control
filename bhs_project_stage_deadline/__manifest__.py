89b5af30bafdbeda313bc1befe78f26c7de635e8
“YenTran” <yentran.bhtech@gmail.com>
2026-05-27 09:02:19 +0700

    [IMP] Upgrade bhs_project_stage_deadline ver 19

---
+++ bhs_project_stage_deadline/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Project Stage Deadline Control',
+    'version': '19.0.0',
+    'summary': 'Control task deadline visibility by project stage',
+    'description': 'Show or hide deadlines on task Kanban cards according to the current project stage.',
+    'author': 'Bac Ha Software',
+    'website': 'https://bachasoftware.com/',
+    'depends': ['project'],
+    'data': [
+        'views/project_task_views.xml',
+        'views/task_stage_views.xml',
+    ],
+    'assets': {
+        'web.assets_frontend': [],
+    },
+    'external_dependencies': {
+        'python': [],
+    },
+    'images': ['static/description/banner.jpg'],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3'
+}
